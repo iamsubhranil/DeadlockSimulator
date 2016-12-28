89cb392fb32bd5a2399c27f960c0ee9113c3e54d
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2016-12-28 19:50:12 +0530

    Initial Commit

---
+++ src/com/iamsubhranil/personal/deadlock/Starter.java
@@ -0,0 +1,24 @@
+/*
+    Created By : iamsubhranil
+    Date : 28/12/16
+    Time : 7:48 PM
+    Package : com.iamsubhranil.personal.deadlock
+    Project : DeadlockSimulator
+*/
+package com.iamsubhranil.personal.deadlock;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        System.setProperty("prism.lcdtext", "false");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
